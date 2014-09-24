58b5808930eba9376fbfc1a99e770b4eb0268ce4
why520crazy <why520crazy@163.com>
2014-09-24 20:02:11 +0800

    add pbox

---
+++ src/pbox.js
@@ -0,0 +1,137 @@
+/*global angular,$,kzi,_ */
+/*jshint unused:false */
+(function () {
+    'use strict';
+    angular.module('wt.pbox', [])
+        .provider("$pbox", [function () {
+            // The default options for all popboxs.
+            var defaultOptions = {
+                placement: 'top',
+                align: null,
+                animation: true,
+                popupDelay: 0,
+                arrow: false,
+                //backdrop       : false,
+                popboxClass: 'popbox',
+                //backdropClass  : 'popbox-backdrop',
+                transitionClass: 'fade',
+                triggerClass: 'in',
+                popboxOpenClass: 'popbox-open',
+                resolve: {},
+                backdropFade: false,
+                popboxFade: false,
+                keyboard: true, // close with esc key
+                backdropClick: true, // only in conjunction with backdrop=true
+                /* other options: template, templateUrl, controller */
+                autoAdapt: false
+            };
+
+            var globalOptions = {}, DEFAULT_OFFSET = 2, FIXED_TOP_HEIGHT = 58;
+
+            this.options = function (value) {
+                globalOptions = value;
+            };
+
+            this.$get = [
+                "$http",
+                "$document",
+                "$compile",
+                "$rootScope",
+                "$controller",
+                "$templateCache",
+                "$q",
+                "$injector",
+                "$timeout",
+                function ($http, $document, $compile, $rootScope, $controller, $templateCache, $q, $injector, $timeout) {
+
+                    var $pbox = {};
+
+                    function getTemplatePromise(options) {
+                        return options.template ? $q.when(options.template) :
+                            $http.get(options.templateUrl, {cache: $templateCache}).then(function (result) {
+                                return result.data;
+                            });
+                    }
+
+                    function getResolvePromises(resolves) {
+                        var promisesArr = [];
+                        angular.forEach(resolves, function (value, key) {
+                            if (angular.isFunction(value) || angular.isArray(value)) {
+                                promisesArr.push($q.when($injector.invoke(value)));
+                            }
+                        });
+                        return promisesArr;
+                    }
+
+                    $pbox.open = function (options) {
+
+                        var pboxResultDeferred = $q.defer();
+                        var pboxOpenedDeferred = $q.defer();
+
+                        //prepare an instance of a modal to be injected into controllers and returned to a caller
+                        var pboxInstance = {
+                            result: pboxResultDeferred.promise,
+                            opened: pboxOpenedDeferred.promise,
+                            close: function (result) {
+                                //$modalStack.close(modalInstance, result);
+                            },
+                            dismiss: function (reason) {
+                                //$modalStack.dismiss(modalInstance, reason);
+                            }
+                        };
+
+                        //merge and clean up options
+                        options = angular.extend({}, defaultOptions, options);
+                        options.resolve = options.resolve || {};
+
+                        //verify options
+                        if (!options.template && !options.templateUrl) {
+                            throw new Error('One of template or templateUrl options is required.');
+                        }
+
+                        var templateAndResolvePromise =
+                            $q.all([getTemplatePromise(options)].concat(getResolvePromises(options.resolve)));
+
+
+                        templateAndResolvePromise.then(function resolveSuccess(tplAndVars) {
+
+                            var pboxScope = (options.scope || $rootScope).$new();
+                            pboxScope.$close = options.close;
+                            pboxScope.$dismiss = options.dismiss;
+
+                            var ctrlInstance, ctrlLocals = {};
+                            var resolveIter = 1;
+
+                            //controllers
+                            if (options.controller) {
+                                ctrlLocals.$scope = pboxScope;
+                                ctrlLocals.$pboxInstance = pboxInstance;
+                                angular.forEach(options.resolve, function (value, key) {
+                                    ctrlLocals[key] = tplAndVars[resolveIter++];
+                                });
+
+                                ctrlInstance = $controller(options.controller, ctrlLocals);
+                            }
+                            var pboxElement = angular.element('<div class="pbox"></div>');
+                            pboxElement.html(tplAndVars[0]);
+                            var outHtml = $compile(pboxElement)(pboxScope);
+                            document.body.appendChild(outHtml[0]);
+
+                        }, function resolveError(reason) {
+                            pboxResultDeferred.reject(reason);
+                        });
+
+                        templateAndResolvePromise.then(function () {
+                            pboxOpenedDeferred.resolve(true);
+                        }, function () {
+                            pboxOpenedDeferred.reject(false);
+                        });
+
+                        return pboxInstance;
+                    };
+
+                    return $pbox;
+                }
+            ];
+        }]);
+})();

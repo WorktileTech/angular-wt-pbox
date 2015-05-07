a24e834a5463e29959c2f13b9e1ce35fe85b29ca
why520crazy <why520crazy@163.com>
2015-05-07 17:54:27 +0800

    pbox init

--- src/pbox.js
+++ src/pbox.js
@@ -3,30 +3,146 @@
 (function () {
     'use strict';
     angular.module('wt.pbox', [])
+        .provider("$wtPosition", [function () {
+            this.$get = ['$document', function ($document) {
+                return {
+                    calculatePos: function (options, $element, $boxElement) {
+                        var elementTop = $element.offset().top,
+                            elementLeft = $element.offset().left,
+                            elementRight = $element.offset().right,
+                            dicOuterWidth = $document.outerWidth(),
+                            dicOuterHeight = $document.outerHeight(),
+                            top, left, right, bottom,
+                            elementOuterWidth = $element.outerWidth(),
+                            elementOuterHeight = $element.outerHeight(),
+                            boxWidth = $boxElement.outerWidth(true),
+                            boxHeight = $boxElement.outerHeight(true);
+                        switch (options.placement) {
+                            case "bottom":
+                                top = elementTop + elementOuterHeight + options.offset;
+                                if (options.align === "left") {
+                                    left = elementLeft;
+                                } else if (options.align === "right") {
+                                    left = elementLeft + elementOuterWidth - boxWidth;
+                                }
+                                else {
+                                    left = elementLeft - boxWidth / 2 + elementOuterWidth / 2;
+                                }
+
+                                if (left < 0) {
+                                    left = options.offset;
+                                }
+                                if (left + boxWidth > dicOuterWidth) {
+                                    left = dicOuterWidth - boxWidth - options.offset;
+                                }
+                                if (top + boxHeight > dicOuterHeight) {
+                                    top = elementTop - boxHeight - options.offset;
+                                }
+                                break;
+                            case "top":
+                                top = elementTop - boxHeight - options.offset;
+                                left = elementLeft;
+                                if (options.align === "left") {
+                                    left = elementLeft;
+                                } else if (options.align === "right") {
+                                    left = elementLeft + elementOuterWidth - boxWidth;
+                                }
+                                else {
+                                    left = elementLeft - boxWidth / 2 + elementOuterWidth / 2;
+                                }
+
+                                if (left < 0) {
+                                    left = options.offset;
+                                }
+                                if (left + boxWidth > dicOuterWidth) {
+                                    left = dicOuterWidth - boxWidth - options.offset;
+                                }
+                                if (top < boxHeight) {
+                                    top = elementTop + elementOuterHeight + options.offset;
+                                }
+                                break;
+                            case "left":
+                                left = elementLeft - boxWidth - options.offset;
+                                if (options.align === "top") {
+                                    top = elementTop;
+                                } else if (options.align === "bottom") {
+                                    top = elementTop + elementOuterHeight - boxHeight;
+                                }
+                                else {
+                                    top = elementTop - boxHeight / 2 + elementOuterHeight / 2;
+                                }
+                                if (left < 0) {
+                                    left = elementLeft + elementOuterWidth + options.offset;
+                                } else if (left + boxWidth > dicOuterWidth) {
+                                    left = elementLeft + elementOuterWidth + options.offset;
+                                }
+                                if (top < 0) {
+                                    top = options.offset;
+                                } else if (top + boxHeight > dicOuterHeight) {
+                                    top = dicOuterHeight - boxHeight - options.offset;
+                                }
+                                break;
+                            case "right":
+                                break;
+                            default:
+                                break;
+                        }
+
+                        if (top !== undefined) {
+                            $boxElement.css("top", top);
+                        }
+                        if (left !== undefined) {
+                            $boxElement.css("left", left);
+                        }
+                        if (right !== undefined) {
+                            $boxElement.css("right", right);
+                        }
+
+                        if (options.animation === true) {
+                            $boxElement.slideToggle("normal");
+                        } else {
+                            $boxElement.addClass(options.openClass);
+                            $boxElement.removeClass(options.closeClass);
+                        }
+                        $element.addClass(options.openClass);
+                    }
+                }
+            }];
+        }])
         .provider("$pbox", [function () {
             // The default options for all popboxs.
             var defaultOptions = {
-                placement: 'top',
-                align: null,
-                animation: true,
+                placement : 'top',
+                align     : null,
+                animation : false,
                 popupDelay: 0,
-                arrow: false,
-                popboxClass: 'popbox',
-                transitionClass: 'fade',
-                triggerClass: 'in',
-                popboxOpenClass: 'popbox-open',
-                resolve: {},
-                backdropFade: false,
-                popboxFade: false,
-                keyboard: true, // close with esc key
-                backdropClick: true, // only in conjunction with backdrop=true
-                autoAdapt: false
+                arrow     : false,
+                openClass : 'pbox-open',
+                closeClass: 'pbox-close',
+                autoClose : true,
+                offset    : 1,
+                resolve   : {}
             };
 
-            var globalOptions = {}, DEFAULT_OFFSET = 2, FIXED_TOP_HEIGHT = 58;
+            var globalOptions = {
+                triggerClass: "pbox-trigger"
+            };
 
             this.options = function (value) {
                 globalOptions = value;
+            };
+
+            var util = {
+                hasClasses: function (element, classes) {
+                    var result = false;
+                    classes.forEach(function (className) {
+                        if (result) {
+                            return;
+                        }
+                        result = element.hasClass(className) || element.parents("." + className).length > 0;
+                    });
+                    return result;
+                }
             };
 
             this.$get = [
@@ -39,9 +155,10 @@
                 "$q",
                 "$injector",
                 "$timeout",
-                function ($http, $document, $compile, $rootScope, $controller, $templateCache, $q, $injector, $timeout) {
+                "$wtPosition",
+                function ($http, $document, $compile, $rootScope, $controller, $templateCache, $q, $injector, $timeout, $wtPosition) {
 
-                    var $pbox = {};
+                    var $pbox = {}, $body = $document.find('body');
 
                     function getTemplatePromise(options) {
                         return options.template ? $q.when(options.template) :
@@ -66,17 +183,36 @@
                         var pboxOpenedDeferred = $q.defer();
                         var pboxElement = null;
 
+                        if (!options.event || !options.event.target) {
+                            throw new Error("The event.target not be null.")
+                        }
+
+                        var $target = angular.element(options.event.target);
+                        var $trigger = $target.parents("." + globalOptions.triggerClass);
+                        if ($trigger.length > 0) {
+                            $target = $trigger;
+                        } else {
+                            $target.addClass(globalOptions.triggerClass);
+                        }
+                        if ($target.data("pboxInstance")) {
+                            return $target.data("pboxInstance").close();
+                        }
+
+                        options.placement = $target.data("placement") ? $target.data("placement") : options.placement;
+
                         //prepare an instance of a modal to be injected into controllers and returned to a caller
                         var pboxInstance = {
-                            result: pboxResultDeferred.promise,
-                            opened: pboxOpenedDeferred.promise,
-                            close: function () {
-                                document.body.removeChild(pboxElement);
+                            result : pboxResultDeferred.promise,
+                            opened : pboxOpenedDeferred.promise,
+                            close  : function () {
+                                $target.removeData("pboxInstance");
+                                pboxElement.remove();
                             },
                             dismiss: function (reason) {
                                 //$modalStack.dismiss(modalInstance, reason);
                             }
                         };
+                        $target.data("pboxInstance", pboxInstance);
 
                         //merge and clean up options
                         options = angular.extend({}, defaultOptions, options);
@@ -104,6 +240,7 @@
                             if (options.controller) {
                                 ctrlLocals.$scope = pboxScope;
                                 ctrlLocals.$pboxInstance = pboxInstance;
+                                pboxScope.$pboxInstance = pboxInstance;
                                 angular.forEach(options.resolve, function (value, key) {
                                     ctrlLocals[key] = tplAndVars[resolveIter++];
                                 });
@@ -111,11 +248,24 @@
                                 ctrlInstance = $controller(options.controller, ctrlLocals);
                                 pboxInstance.ctrlInstance = ctrlInstance;
                             }
-                            var outerElement = angular.element('<div class="pbox"></div>');
-                            outerElement.html(tplAndVars[0]);
-                            outerElement = $compile(outerElement)(pboxScope);
-                            pboxElement = outerElement[0];
-                            document.body.appendChild(outerElement[0]);
+                            pboxElement = angular.element('<div class="pbox"></div>');
+                            pboxElement.html(tplAndVars[0]);
+                            //outerElement.css({display: 'none'});
+                            //pboxInstance._boxElement = outerElement;
+
+                            $document.bind("mousedown.pbox", function (e) {
+                                var _target = angular.element(e.target);
+                                if (util.hasClasses(_target, ["pbox", globalOptions.triggerClass])) {
+                                    return;
+                                }
+                                $document.unbind("mousedown.pbox");
+                                pboxInstance.close();
+                            });
+
+                            $compile(pboxElement)(pboxScope);
+                            $body.append(pboxElement);
+
+                            $wtPosition.calculatePos(options, $target, pboxElement);
 
                         }, function resolveError(reason) {
                             pboxResultDeferred.reject(reason);

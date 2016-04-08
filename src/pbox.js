527d47d3164bfef41959da64163ffabf4eceb455
why520crazy <why520crazy@163.com>
2016-04-08 10:53:39 +0800

    解决原型方法作用域的问题

--- src/pbox.js
+++ src/pbox.js
@@ -150,23 +150,23 @@
         .provider("$pbox", [function () {
             // The default options for all popboxs.
             var defaultOptions = {
-                placement : 'bottom',
-                align     : null, //居什么对齐 left,right,bottom,top
-                animation : false,//是否有动画
-                delay     : 0,       //延迟多长时间弹出
-                arrow     : false,
-                openClass : 'pbox-open',
+                placement: 'bottom',
+                align: null, //居什么对齐 left,right,bottom,top
+                animation: false,//是否有动画
+                delay: 0,       //延迟多长时间弹出
+                arrow: false,
+                openClass: 'pbox-open',
                 closeClass: 'pbox-close',
-                autoClose : true, //点击其他区域自动关闭，当点击触发弹出框的元素或者弹出框内部元素时不关闭，
+                autoClose: true, //点击其他区域自动关闭，当点击触发弹出框的元素或者弹出框内部元素时不关闭，
                 forceClose: false,//autoClose为true时起作用，点击触发弹出框的元素也关闭
-                offset    : 1,    //位移位置
-                autoAdapt : true, //是否自动计算上下，左右的高度或者宽度，当 placement 为 bottom，top的时候为true，自动调整 placement
-                watch     : false,//watch 弹出框的宽高，当有变化的时候重新计算位置
-                resolve   : {}
+                offset: 1,    //位移位置
+                autoAdapt: true, //是否自动计算上下，左右的高度或者宽度，当 placement 为 bottom，top的时候为true，自动调整 placement
+                watch: false,//watch 弹出框的宽高，当有变化的时候重新计算位置
+                resolve: {}
             };
 
             var globalOptions = {
-                triggerClass   : "pbox-trigger",
+                triggerClass: "pbox-trigger",
                 boxInstanceName: "boxInstance"
             };
 
@@ -175,7 +175,7 @@
             };
 
             var util = {
-                hasClass  : function (element, className) {
+                hasClass: function (element, className) {
                     return element.hasClass(className) || element.parents("." + className).length > 0;
                 },
                 hasClasses: function (element, classes) {
@@ -188,7 +188,7 @@
                     });
                     return result;
                 },
-                getTarget : function (event) {
+                getTarget: function (event) {
                     var $target = angular.element(event.target);
                     if (!$target) {
                         throw new Error("The event")
@@ -239,18 +239,15 @@
                     }
 
                     function BoxModal(options, $target) {
-                        var _resultDeferred = $q.defer();
-                        var _openedDeferred = $q.defer();
-                        var _self = this;
                         this._id = new Date().getTime() + Math.random().toString(36).substr(2);
-                        this.resultDeferred = _resultDeferred;
-                        this.openedDeferred = _openedDeferred;
-                        this.result = _resultDeferred.promise;
-                        this.opened = _openedDeferred.promise;
+                        this.resultDeferred = $q.defer();
+                        this.openedDeferred = $q.defer();
+                        this.result = this.resultDeferred.promise;
+                        this.opened = this.openedDeferred.promise;
                         this._options = options;
                         this._pboxElement = null;
                         this._$target = $target;
-                        $target.data(globalOptions.boxInstanceName, _self);
+                        $target.data(globalOptions.boxInstanceName, this);
 
                         BoxModal.prototype._remove = function () {
                             this._$target.removeData(globalOptions.boxInstanceName);
@@ -259,6 +256,7 @@
                         };
 
                         BoxModal.prototype._bindEvents = function () {
+                            var _self = this;
                             if (_self._options.autoClose) {
                                 _self._pboxElement.bind("mousedown.pbox", function (e) {
                                     e.stopPropagation();
@@ -297,6 +295,7 @@
                         };
 
                         BoxModal.prototype.open = function (tpl, scope) {
+                            var _self = this;
                             this._pboxElement = angular.element('<div class="pbox"></div>');
                             this._pboxElement.html(tpl);
                             this._$target.addClass(this._options.openClass);
